000c6f429acc6a2bf6d8f3a3124581f748b29a85
jeancoravu <jean.coravu@c-s.ro>
2020-06-18 17:20:23 +0300

    Update the log messages in the RunProductReaderTest class.

--- src/test/java/org/esa/snap/dataio/RunProductReaderTest.java
+++ src/test/java/org/esa/snap/dataio/RunProductReaderTest.java
@@ -5,7 +5,6 @@ import org.esa.snap.core.dataio.ProductReader;
 import org.esa.snap.core.dataio.ProductReaderPlugIn;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.util.SystemUtils;
-import org.esa.snap.gpt.RunGPTProductReaderTest;
 import org.esa.snap.lib.openjpeg.activator.OpenJPEGInstaller;
 import org.esa.snap.runtime.LogUtils;
 import org.junit.BeforeClass;
@@ -16,8 +15,8 @@ import org.junit.rules.ErrorCollector;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -26,35 +25,52 @@ import static org.junit.Assert.fail;
 
 public class RunProductReaderTest {
 
-    private static final String PROPERTYNAME_FAIL_ON_MISSING_DATA = "snap.reader.tests.failOnMissingData";
-    private static final String PROPERTY_NAME_DATA_DIR = "snap.reader.tests.data.dir";
+    private static final Logger logger = Logger.getLogger(RunProductReaderTest.class.getName());
 
+    private static final String PROPERTY_NAME_FAIL_ON_MISSING_DATA = "snap.reader.tests.failOnMissingData";
+    private static final String PROPERTY_NAME_DATA_DIR = "snap.reader.tests.data.dir";
     private static final String PROPERTY_NAME_CASS_NAME = "snap.reader.tests.class.name";
-    private static final boolean FAIL_ON_MISSING_DATA = Boolean.parseBoolean(System.getProperty(PROPERTYNAME_FAIL_ON_MISSING_DATA, "true"));
-    private static final String INDENT = "\t";
+    private static final String PROPERTY_NAME_PRODUCT_ID = "snap.reader.tests.product.id";
+
+    private static final boolean FAIL_ON_MISSING_DATA = Boolean.parseBoolean(System.getProperty(PROPERTY_NAME_FAIL_ON_MISSING_DATA, "true"));
 
     private static TestDefinitionList testDefinitionList;
     private static File dataRootDir;
 
-    private static final Logger logger = Logger.getLogger(RunProductReaderTest.class.getName());
-
     @Rule
     public ErrorCollector errorCollector = new ErrorCollector();
-    private static final ProductList testProductList = new ProductList();
 
     public RunProductReaderTest() {
     }
 
     @BeforeClass
     public static void initialize() throws Exception {
+        assertRequiredTestDataDirectory();
+
         LogUtils.initLogger();
 
-        logFailOnMissingDataMessage();
+        String readerPlugInClassNameToTest = System.getProperty(PROPERTY_NAME_CASS_NAME);
+        String productIdToTest = System.getProperty(PROPERTY_NAME_PRODUCT_ID);
 
-        assertTestDataDirectory();
-        testDefinitionList = loadProductReaderTestDefinitions();
+        if (logger.isLoggable(Level.FINE)) {
+            StringBuilder message = new StringBuilder();
+            message.append("Input test parameters: data folder path: ")
+                    .append(dataRootDir.getAbsolutePath())
+                    .append(", reader plugin class: ")
+                    .append(readerPlugInClassNameToTest)
+                    .append(", product id :")
+                    .append(productIdToTest)
+                    .append(".");
+            logger.log(Level.FINE, message.toString());
+        }
 
-        createGlobalProductList();
+        SystemUtils.init3rdPartyLibs(RunProductReaderTest.class);
+
+        if (!FAIL_ON_MISSING_DATA) {
+            logger.warning("Tests will not fail if test data is missing!");
+        }
+
+        testDefinitionList = loadProductReaderTestDefinitions(readerPlugInClassNameToTest, productIdToTest);
 
         OpenJPEGInstaller.install();
     }
@@ -64,25 +80,58 @@ public class RunProductReaderTest {
         logger.info("Test product content for " + testDefinitionList.size() + " reader plugins.");
 
         for (TestDefinition testDefinition : testDefinitionList) {
-            logger.info("Start testing reader plugin: " + testDefinition.getProductReaderPlugin().getClass().getSimpleName() + ".");
-            long startTime = System.currentTimeMillis();
-
             final List<String> intendedProductIds = testDefinition.getDecodableProductIds();
+            String readerPluginClassName = testDefinition.getProductReaderPlugin().getClass().getSimpleName();
+
+            logger.info("Test products content: reader plugin class: " + readerPluginClassName+ ", product count: " + intendedProductIds.size()+".");
+
             for (String productId : intendedProductIds) {
-                final TestProduct testProduct = testProductList.getById(productId);
-                String reason = "Test file not defined for ID=" + productId;
-                errorCollector.checkThat(reason, testProduct, is(notNullValue()));
-                if (testProduct == null) {
-                    continue;
+                TestProduct foundTestProduct = null;
+                for (TestProduct testProduct : testDefinition.getAllProducts()) {
+                    if (testProduct.getId().equalsIgnoreCase(productId)) {
+                        foundTestProduct = testProduct;
+                        break;
+                    }
                 }
-                if (testProduct.exists()) {
-                    final File testProductFile = getTestProductFile(testProduct);
+                if (foundTestProduct == null) {
+                    String message = "The test product with id '"+ productId+"' does not exist.";
+                    if (FAIL_ON_MISSING_DATA) {
+                        fail(message);
+                    } else {
+                        logger.info(message);
+                    }
+                } else if (foundTestProduct.exists()) {
+                    logger.info("Start testing the product content: reader plugin class: " + readerPluginClassName+ ", product id: " + foundTestProduct.getId()+", product relative path: " + foundTestProduct.getRelativePath()+".");
+
+                    long startTime = System.currentTimeMillis();
+
+                    final File testProductFile = new File(dataRootDir, foundTestProduct.getRelativePath());
+
+                    long startTimeReadProduct = System.currentTimeMillis();
 
                     final ProductReader productReader = testDefinition.getProductReaderPlugin().createReaderInstance();
-
                     final Product product = productReader.readProductNodes(testProductFile, null);
                     try {
-                        assertExpectedContent(testDefinition, productId, product);
+                        if (logger.isLoggable(Level.FINE)) {
+                            double elapsedSecondsReadProduct = (System.currentTimeMillis() - startTimeReadProduct) / 1000.0d;
+                            StringBuilder message = new StringBuilder();
+                            message.append("Finish reading the product id: ")
+                                    .append(foundTestProduct.getId())
+                                    .append(", relative path: ")
+                                    .append(foundTestProduct.getRelativePath())
+                                    .append(", reader class: ")
+                                    .append(productReader.getClass().getName())
+                                    .append(", size: ")
+                                    .append(product.getSceneRasterWidth())
+                                    .append("x")
+                                    .append(product.getSceneRasterHeight())
+                                    .append(", elapsed time: ")
+                                    .append(elapsedSecondsReadProduct)
+                                    .append(" seconds.");
+                            logger.log(Level.FINE, message.toString());
+                        }
+
+                        assertExpectedContent(testDefinition, foundTestProduct, product);
                     } catch (Throwable t) {
                         errorCollector.addError(new Throwable("[" + productId + "] " + t.getMessage(), t));
                     } finally {
@@ -90,81 +139,64 @@ public class RunProductReaderTest {
                             product.dispose();
                         }
                     }
+
+                    if (logger.isLoggable(Level.FINE)) {
+                        double elapsedSeconds = (System.currentTimeMillis() - startTime) / 1000.0d;
+                        StringBuilder message = new StringBuilder();
+                        message.append("Finish testing the product content id: ")
+                                .append(foundTestProduct.getId())
+                                .append(", relative path: ")
+                                .append(foundTestProduct.getRelativePath())
+                                .append(", elapsed time: ")
+                                .append(elapsedSeconds)
+                                .append(" seconds.");
+                        logger.log(Level.FINE, message.toString());
+                    }
                 } else {
-                    logProductNotExistent(2, testProduct);
-                }
-            }
-            double elapsedSeconds = (System.currentTimeMillis() - startTime) / 1000.0d;
-            logger.info("Finish testing reader plugin: " + testDefinition.getProductReaderPlugin().getClass().getSimpleName() + ". The time elapsed is " + elapsedSeconds + " seconds.");
-        }
-    }
-
-    private static void createGlobalProductList() {
-        for (TestDefinition testDefinition : testDefinitionList) {
-            final List<TestProduct> allPluginProducts = testDefinition.getAllProducts();
-            for (TestProduct testProduct : allPluginProducts) {
-                if (!testIfIdAlreadyRegistered(testProduct)) {
-                    testProductList.add(testProduct);
+                    logger.info("The test product with id '"+ foundTestProduct.getId()+"' is missing from the disc.");
                 }
             }
         }
     }
 
-    private static boolean testIfIdAlreadyRegistered(TestProduct testProduct) {
-        final String id = testProduct.getId();
-        final TestProduct storedProduct = testProductList.getById(id);
-        if (storedProduct != null) {
-            if (storedProduct.isDifferent(testProduct)) {
-                fail("Test file with ID=" + id + " already defined with different settings");
-            }
-
-            return true;
-        }
-
-        return false;
-    }
-
-    private void logProductNotExistent(int indention, TestProduct testProduct) {
-        final StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < indention; i++) {
-            sb.append(INDENT);
-        }
-        logger.info(sb.toString() + "Not existent - " + testProduct.getId());
-    }
-
-    private static void assertExpectedContent(TestDefinition testDefinition, String productId, Product product) {
-        final ExpectedContent expectedContent = testDefinition.getExpectedContent(productId);
+    private static void assertExpectedContent(TestDefinition testDefinition, TestProduct testProduct, Product product) {
+        final ExpectedContent expectedContent = testDefinition.getExpectedContent(testProduct.getId());
         if (expectedContent == null) {
-            logger.info("No expected content for product id '" + productId+"'.");
+            String message = "No expected content for product id '" + testProduct.getId()+"'.";
+            if (FAIL_ON_MISSING_DATA) {
+                fail(message);
+            } else {
+                logger.info(message);
+            }
         } else {
             long startTime = System.currentTimeMillis();
-            logger.info("Start testing expected content: product id: " + productId+".");
 
-            final ContentAssert contentAssert = new ContentAssert(expectedContent, productId, product);
+            final ContentAssert contentAssert = new ContentAssert(expectedContent, testProduct.getId(), product);
             contentAssert.assertProductContent();
 
-            double elapsedSeconds = (System.currentTimeMillis() - startTime) / 1000.0d;
-            logger.info("Finish testing expected content: product id: " + productId+"'. The time elapsed is " + elapsedSeconds + " seconds.");
+            if (logger.isLoggable(Level.FINE)) {
+                double elapsedSeconds = (System.currentTimeMillis() - startTime) / 1000.0d;
+                StringBuilder message = new StringBuilder();
+                message.append("Finish testing expected content: product id: ")
+                        .append(testProduct.getId())
+                        .append(", relative path: ")
+                        .append(testProduct.getRelativePath())
+                        .append(", elapsed time: ")
+                        .append(elapsedSeconds)
+                        .append(" seconds.");
+                logger.log(Level.FINE, message.toString());
+            }
         }
     }
 
-    private File getTestProductFile(TestProduct testProduct) {
-        final String relativePath = testProduct.getRelativePath();
-        final File testProductFile = new File(dataRootDir, relativePath);
-
-        errorCollector.checkThat("testProductFile exist " + testProduct.getId(), testProductFile.exists(), is(true));
-        return testProductFile;
-    }
-
-    private static TestDefinitionList loadProductReaderTestDefinitions() throws IOException {
+    private static TestDefinitionList loadProductReaderTestDefinitions(String readerPlugInClassNameToTest, String productIdToTest) throws IOException {
         final ObjectMapper mapper = new ObjectMapper();
         final Iterable<ProductReaderPlugIn> readerPlugIns = SystemUtils.loadServices(ProductReaderPlugIn.class);
-        final String className = System.getProperty(PROPERTY_NAME_CASS_NAME);
 
         TestDefinitionList testDefinitionList = new TestDefinitionList();
         for (ProductReaderPlugIn readerPlugIn : readerPlugIns) {
             final Class<? extends ProductReaderPlugIn> readerPlugInClass = readerPlugIn.getClass();
-            if (className != null && !readerPlugInClass.getName().startsWith(className)) {
+            if (readerPlugInClassNameToTest != null && !readerPlugInClass.getName().startsWith(readerPlugInClassNameToTest)) {
                 continue;
             }
 
@@ -172,58 +204,52 @@ public class RunProductReaderTest {
             final String dataResourceName =  "/" + path + "-data.json";
             final URL dataResource = readerPlugInClass.getResource(dataResourceName);
             if (dataResource == null) {
-                logger.warning(readerPlugInClass.getSimpleName() + " does not define test data");
+                logger.warning("The reader plugin class '"+readerPlugInClass.getSimpleName() + "' does not define test data.");
                 continue;
             }
 
+            final ProductList productList = mapper.readValue(dataResource, ProductList.class);
+
             final TestDefinition testDefinition = new TestDefinition();
             testDefinition.setProductReaderPlugin(readerPlugIn);
-
-            final ProductList productList = mapper.readValue(dataResource, ProductList.class);
-            testIfProductFilesExists(productList);
-            testDefinition.addTestProducts(productList.getAll());
-
-            for (TestProduct product : productList.getAll()) {
-                final String fileResourceName = product.getId() + ".json";
-                final URL fileResource = readerPlugInClass.getResource(fileResourceName);
-                if (fileResource == null) {
-                    fail(readerPlugInClass.getSimpleName() + " resource file '" + fileResourceName + "' is missing");
+            for (TestProduct testProduct : productList.getAll()) {
+                if (productIdToTest == null || productIdToTest.equalsIgnoreCase(testProduct.getId())) {
+                    final File productFile = new File(dataRootDir, testProduct.getRelativePath());
+                    if (!productFile.exists()) {
+                        testProduct.exists(false);
+                        String message = "The product '" + productFile.getAbsolutePath()+"' of the reader plugin '"+ readerPlugInClass.getSimpleName()+"' does not exist.";
+                        if (FAIL_ON_MISSING_DATA) {
+                            fail(message);
+                        } else {
+                            logger.info(message);
+                        }
+                    }
+                    testDefinition.getAllProducts().add(testProduct);
+                    final String fileResourceName = testProduct.getId() + ".json";
+                    final URL fileResource = readerPlugInClass.getResource(fileResourceName);
+                    if (fileResource == null) {
+                        fail("The resource file '" + fileResourceName+"' of the reader plugin '"+ readerPlugInClass.getSimpleName()+"' does not exist.");
+                    }
+                    final ExpectedDataset expectedDataset = mapper.readValue(fileResource, ExpectedDataset.class);
+                    testDefinition.addExpectedDataset(expectedDataset);
                 }
-                final ExpectedDataset expectedDataset = mapper.readValue(fileResource, ExpectedDataset.class);
-                testDefinition.addExpectedDataset(expectedDataset);
             }
             testDefinitionList.add(testDefinition);
         }
         return testDefinitionList;
     }
 
-    private static void assertTestDataDirectory() {
-        final String dataDirProperty = System.getProperty(PROPERTY_NAME_DATA_DIR);
-        if (dataDirProperty == null) {
-            fail("Data directory path not set");
+    private static void assertRequiredTestDataDirectory() {
+        String dataDirPropertyValue = System.getProperty(PROPERTY_NAME_DATA_DIR);
+        if (dataDirPropertyValue == null) {
+            fail("The test data property '"+PROPERTY_NAME_DATA_DIR+"' representing the folder which contains the source products is not set.");
         }
-        dataRootDir = new File(dataDirProperty);
+        dataRootDir = new File(dataDirPropertyValue);
+        if (!dataRootDir.exists()) {
+            fail("The path '" + dataDirPropertyValue+"' representing the folder which contains the source products does not exist.");
+        }
         if (!dataRootDir.isDirectory()) {
-            fail("Data directory is not valid: " + dataDirProperty);
-        }
-    }
-
-    private static void testIfProductFilesExists(ProductList productList) {
-        for (TestProduct testProduct : productList) {
-            final String relativePath = testProduct.getRelativePath();
-            final File productFile = new File(dataRootDir, relativePath);
-            if (!productFile.exists()) {
-                testProduct.exists(false);
-                if (FAIL_ON_MISSING_DATA) {
-                    fail("Test product does not exist: " + productFile.getAbsolutePath());
-                }
-            }
-        }
-    }
-
-    private static void logFailOnMissingDataMessage() {
-        if (!FAIL_ON_MISSING_DATA) {
-            logger.warning("Tests will not fail if test data is missing!");
+            fail("The path '" + dataDirPropertyValue+"' exists and it does not denote a folder.");
         }
     }
 }
