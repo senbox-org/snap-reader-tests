0cae404e93e6fe9b169daaa04c23624181b8dfd4
Florian1209 <douziechflorian@gmail.com>
2021-08-02 11:51:10 +0200

    update windows jenkins scripts

--- jenkins_windows/launchReaderTest.sh
+++ jenkins_windows/launchReaderTest.sh
@@ -14,9 +14,10 @@ export DATA_PATH=$1
 export CLASS_PATH_FILTER=$2
 export BUILD_NUMBER=$3
 export RAM_SIZE=$4
-export SNAP_LIB_DIR='C:/Program Files/snap/snap/modules/lib/amd64'
+export SNAP_LIB_DIR="C:/Program Files/snap/snap/modules/lib/x86"
 export LD_LIBRARY_PATH=.
 export MAVEN_OPTS="-Xmx${RAM_SIZE}"
-export MAX_MEMORY="-Xmx${RAM_SIZE}" 
+export MAX_MEMORY="-Xmx${RAM_SIZE}"
+
 # Launch tests
-mvn -Dmax.memory=${MAX_MEMORY} -Dncsa.hdf.hdflib.HDFLibrary.hdflib=${SNAP_LIB_DIR}/libjhdf.so -Dncsa.hdf.hdf5lib.H5.hdf5lib=${SNAP_LIB_DIR}/libjhdf5.so -Dsnap.userdir='C:/Program Files/snap' -Dsnap.reader.tests.execute=true -Dsnap.reader.tests.data.dir=${DATA_PATH} -Dsnap.reader.tests.class.name=${CLASS_PATH_FILTER} -Dsnap.reader.tests.failOnMissingData=false clean test 2>&1 | tee -a ./readerTest-${BUILD_NUMBER}.log
+mvn -Dmax.memory=${MAX_MEMORY} -Dncsa.hdf.hdflib.HDFLibrary.hdflib="${SNAP_LIB_DIR}"/libjhdf.so -Dncsa.hdf.hdf5lib.H5.hdf5lib="${SNAP_LIB_DIR}"/libjhdf5.so -Dsnap.userdir='C:/Program Files/snap' -Dsnap.reader.tests.execute=true -Dsnap.reader.tests.data.dir="${DATA_PATH}" -Dsnap.reader.tests.class.name="${CLASS_PATH_FILTER}" -Dsnap.reader.tests.failOnMissingData=false clean test 2>&1 | tee -a ./readerTest-${BUILD_NUMBER}.log
