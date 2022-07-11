f8a40641a7c4d72ba86a94b4668343b7318f9237
Florian1209 <douziechflorian@gmail.com>
2022-07-11 15:18:23 +0200

    update CI windows script

--- jenkins_windows/setUpLibraries.sh
+++ jenkins_windows/setUpLibraries.sh
@@ -11,8 +11,8 @@ echo "Set up OpenJPEG"
 rm -rf C:/Users/admin/setupLib/tmp
 mkdir C:/Users/admin/setupLib/tmp
 cd C:/Users/admin/setupLib/tmp
-cp C:/Temp/snap/.m2/repository/org/esa/snap/lib-openjpeg/9.0.0-SNAPSHOT/lib-openjpeg-9.0.0-SNAPSHOT.jar .
-export OPENJPEG_VERSION="9.0.0-SNAPSHOT"
+cp C:/Users/admin/.m2/org/esa/snap/lib-openjpeg/10.0.0-SNAPSHOT/lib-openjpeg-10.0.0-SNAPSHOT.jar .
+export OPENJPEG_VERSION="10.0.0-SNAPSHOT"
 unzip -o lib-openjpeg-${OPENJPEG_VERSION}.jar
 mkdir -p C:/Users/admin/.snap/auxdata/openjpeg/${OPENJPEG_VERSION}
 cp -r auxdata/openjpeg/* C:/Users/admin/.snap/auxdata/openjpeg/${OPENJPEG_VERSION}
@@ -20,8 +20,8 @@ chmod 755 -R C:/Users/admin/.snap/auxdata/openjpeg/${OPENJPEG_VERSION}/*/bin
 
 # Set up GDAL
 echo "Set up GDAL"
-cp C:/Temp/snap/.m2/repository/org/esa/s2tbx/lib-gdal/9.0.0-SNAPSHOT/lib-gdal-9.0.0-SNAPSHOT.jar .
-export GDAL_VERSION="9.0.0-SNAPSHOT"
+cp C:/Users/admin/.m2/org/esa/s2tbx/lib-gdal/10.0.0-SNAPSHOT/lib-gdal-10.0.0-SNAPSHOT.jar .
+export GDAL_VERSION="10.0.0-SNAPSHOT"
 unzip -o lib-gdal-${GDAL_VERSION}.jar
 mkdir -p C:/Users/admin/.snap/auxdata/gdal
 cp auxdata/gdal/Windows/x64/gdal-*-*-1.zip C:/Users/admin/.snap/auxdata/gdal
